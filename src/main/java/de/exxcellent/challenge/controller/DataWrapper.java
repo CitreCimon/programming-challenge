ec43808095e6e857f425c2eb480c3571f4ea7f20
Simon David Hoffmann <simon-david.hoffmann@stud.uni-bamberg.de>
2019-07-01 20:25:49 +0200

    added interface and exception

--- src/main/java/de/exxcellent/challenge/controller/DataWrapper.java
+++ src/main/java/de/exxcellent/challenge/controller/DataWrapper.java
@@ -2,4 +2,9 @@ package de.exxcellent.challenge.controller;
 
 public interface DataWrapper {
 
+	void ReadDataFromCSVandStoreinOODB(String pathtofile) throws ExceptionChallenge ;
+	void mapCSVEntrytoOO(String line) throws ExceptionChallenge ;
+	void checkstructure(String line, String structure) throws ExceptionChallenge;
+	void checknumberofColumns(String[] linearr, int numberofColumns) throws ExceptionChallenge;
+
 }
--- src/main/java/de/exxcellent/challenge/exception/ExceptionChallenge.java
+++ src/main/java/de/exxcellent/challenge/exception/ExceptionChallenge.java
@@ -1,5 +1,15 @@
 package de.exxcellent.challenge.exception;
 
-public class ExceptionChallenge {
+public class ExceptionChallenge extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public ExceptionChallenge(String message){
+		super(message);
+	}
+	
+	
 }
